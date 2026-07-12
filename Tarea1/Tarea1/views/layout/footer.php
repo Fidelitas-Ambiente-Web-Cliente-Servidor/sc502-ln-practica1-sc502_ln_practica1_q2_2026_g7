c1753c106fe6ed777ff70c2038a64f7ac061fbf5
coffelatte05 <ashchavacal05@gmail.com>
2026-07-12 00:12:11 -0600

    Se agrego el header y footer

---
+++ Tarea1/Tarea1/views/layout/footer.php
@@ -0,0 +1,9 @@
+    <footer>
+        <p>Academia NovaTech</p>
+        <p>Facebook | Instagram | TikTok</p>
+    </footer>
+
+    <script src="./js/index.js"></script>
+
+</body>
+</html>
---
+++ Tarea1/Tarea1/views/layout/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Academia NovaTech</title>
+    <link rel="stylesheet" href="./css/index.css">
+</head>
+<body>
+
+    <header>
+        <nav>
+            <h1>Academia NovaTech</h1>
+
+            <ul>
+                <li><a class="activo" href="index.html">Inicio</a></li>
+                <li><a href="cursos.html">Cursos</a></li>
+                <li><a href="profesores.html">Profesores</a></li>
+                <li><a href="contacto.html">Contacto</a></li>
+            </ul>
+        </nav>
+    </header>
