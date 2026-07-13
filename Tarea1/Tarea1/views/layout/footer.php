0ad63f1060da3a6b04fc24e1ef34bba95575fa90
nay-araya12 <nayarayat@gmail.com>
2026-07-13 15:14:37 -0600

    commit nay: se modifico el footer y el header

--- Tarea1/Tarea1/views/layout/footer.php
+++ Tarea1/Tarea1/views/layout/footer.php
@@ -1,9 +1,7 @@
-    <footer>
-        <p>Academia NovaTech</p>
-        <p>Facebook | Instagram | TikTok</p>
-    </footer>
-
-    <script src="./js/index.js"></script>
+<footer>
+    <p>Academia NovaTech</p>
+    <p>Facebook | Instagram | TikTok</p>
+</footer>
 
 </body>
 </html>
--- Tarea1/Tarea1/views/layout/header.php
+++ Tarea1/Tarea1/views/layout/header.php
@@ -1,21 +1,52 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
+
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0"
+    >
+
     <title>Academia NovaTech</title>
-    <link rel="stylesheet" href="./css/index.css">
+
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/cursos.css">
 </head>
+
 <body>
 
-    <header>
-        <nav>
-            <h1>Academia NovaTech</h1>
+<header>
+    <nav>
 
-            <ul>
-                <li><a class="activo" href="index.html">Inicio</a></li>
-                <li><a href="cursos.html">Cursos</a></li>
-                <li><a href="profesores.html">Profesores</a></li>
-                <li><a href="contacto.html">Contacto</a></li>
-            </ul>
-        </nav>
-    </header>
+        <h1>Academia NovaTech</h1>
+
+        <ul>
+            <li>
+                <a href="index.php?controller=index&action=index">
+                    Inicio
+                </a>
+            </li>
+
+            <li>
+                <a href="index.php?controller=cursos&action=index">
+                    Cursos
+                </a>
+            </li>
+
+            <li>
+                <a href="index.php?controller=profesores&action=index">
+                    Profesores
+                </a>
+            </li>
+
+            <li>
+                <a href="index.php?controller=contacto&action=index">
+                    Contacto
+                </a>
+            </li>
+        </ul>
+
+    </nav>
+</header>
